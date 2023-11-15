8e008143fdbcb74a6e99ffa7a65dff7a75367c97
JinLee794 <94473824+JinLee794@users.noreply.github.com>
2023-11-15 09:22:21 -0600

    adding logic to get nested AAD group user membership details, added requirements.txt

--- adoTeamsQueryDemo/main.py
+++ adoTeamsQueryDemo/main.py
@@ -4,6 +4,8 @@ import base64
 import sys
 import pandas as pd
 import os
+from IPython.display import display
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -33,14 +35,13 @@ data = []
 for team in org_teams_response.json()['value']:
   # Initialize the JSON object to store the team membership info
   team_info = {
-    'teamName': team['name'],
-    'projectName': team['projectName'],
-    'teamID': team['id'],
-    'projectID': team['projectId']
+    'Team Name': team['name'],
+    'Project Name': team['projectName'],
+    'Team ID': team['id'],
+    'Project ID': team['projectId']
   }
 
   # Get the members of the team
-  # GET https://dev.azure.com/{organization}/_apis/projects/{projectId}/teams/{teamId}/members?api-version=7.1-preview.2
   team_members_url = f"https://dev.azure.com/{ORG}/_apis/projects/{team['projectId']}/teams/{team['id']}/members?api-version=7.1-preview.2"
   try:
     team_members_response = requests.get(team_members_url, headers=headers, timeout=10)
@@ -50,12 +51,38 @@ for team in org_teams_response.json()['value']:
   
   for member in team_members_response.json()['value']:
     row = team_info.copy()
-    row.update({
-      'displayName': member['identity']['displayName'],
-      'uniqueName': member['identity']['uniqueName'],
-      'id': member['identity']['id']
-    })
+    # If the unique name contains vsts:/// then it is a group and not a user. Do a nested call to get all user memberships
+    if 'vstfs:' in member['identity']['uniqueName']:
+      group_membership_url = f"https://vsaex.dev.azure.com/{ORG}/_apis/GroupEntitlements/{member['identity']['id']}/members?api-version=7.1-preview.1"
+      try:
+        group_members_response = requests.get(group_membership_url, headers=headers, timeout=10)
+      except requests.exceptions.RequestException as e:
+        print(f"An error occurred while getting the members of the group {member['identity']['uniqueName']}: {e}")
+        continue
+      
+      # For each of the group members, add them as a row to the dataframe
+      for group_member in group_members_response.json()['members']:
+        row.update({
+          'Group Display Name': member['identity']['displayName'],
+          'Group Unique Name': member['identity']['uniqueName'],
+          'Group ID': member['identity']['id'],
+          'User Display Name': group_member['user']['displayName'],
+          'User Unique Name': group_member['user']['principalName'],
+          'User Origin': group_member['user']['origin'],
+          'User ID': group_member['user']['originId']
+        })
+    else:
+      # If the unique name does not contain vsts:/// then it is a user. Add them as a row to the dataframe
+      row.update({
+        'User Origin': 'ado',
+        'User Display Name': member['identity']['displayName'],
+        'User Unique Name': member['identity']['uniqueName'],
+        'User ID': member['identity']['id']
+      })
     data.append(row)
 
 df = pd.DataFrame(data)
+df = df[['Project Name', 'Project ID', 'Team Name', 'Team ID', 'User Display Name', 'User Unique Name', 'User ID', 'User Origin', 'Group Display Name', 'Group Unique Name', 'Group ID']]
+# display(df.to_markdown()) # To debug dataframe output locally
+
 print(df)
